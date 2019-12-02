792ff20049134ba0ff77d9bbf40d83fdca337a1d
Arthur_H <arthurh@github.com>
2019-12-02 01:12:49 -0500

    update

--- depth.py
+++ depth.py
@@ -28,17 +28,17 @@ if __name__ == "__main__":
     im_left_path = "train/image_left/um_000000.jpg"
     im_right_path = "train/image_right/um_000000.jpg"
     # read images
-    im_left = io.imread(im_left_path, as_gray=True)
+    im_left = io.imread(im_left_path)
     # im_left = cv2.imread(im_left_path,cv2.IMREAD_GRAYSCALE)
     #im_left = cv2.cvtColor(im_left, cv2.COLOR_BGR2RGB)
-    im_right = io.imread(im_right_path, as_gray=True)
+    im_right = io.imread(im_right_path)
     #im_right = cv2.imread(im_right_path,cv2.IMREAD_GRAYSCALE)
     #im_right = cv2.cvtColor(im_right, cv2.COLOR_BGR2RGB)
 
     # compute disparity map
     disparity = compute_disparity(im_left, im_right)
 
-    calib_file_dir = './train/calib/um_000000.txt'
+    calib_file_dir = 'train/calib/um_000000.txt'
     f,T,px,py,depth = compute_depth(calib_file_dir, disparity)
     # plot depth map
     fig=plt.figure(figsize=(10, 10))
