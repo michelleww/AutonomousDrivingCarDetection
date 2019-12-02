9674ee1da40f7bb1d3ec17ca2ce58311739769a7
Luya Wang <luya.wang@mail.utoronto.ca>
2019-12-02 06:00:17 -0500

    update

--- road_detection.py
+++ road_detection.py
@@ -5,13 +5,12 @@ from matplotlib import pyplot as plt
 from skimage.segmentation import slic
 from skimage.measure import perimeter
 import os
-# from keras.models import Sequential
-# from keras.models import model_from_json
-# from keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Dropout
-from sklearn.model_selection import train_test_split
 from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 import joblib
 import mahotas as mt
+import matplotlib.colors as mcolors
+import webcolors
 
 def extract_feature(image, image_gt):
     # calculate image gradient using sobel
@@ -83,9 +82,7 @@ def extract_test_feature(image):
     img_seg = slic(image, n_segments = 1000, compactness=8, convert2lab=True, min_size_factor=0.3)
     # actual total number of labels
     num_labels = np.amax(img_seg)+1
-    # print(la)
-    # print(np.unique(img_seg))
-    # print(num_labels)
+
     bins = np.linspace(0,1,num=21)
 
     features_list = []
@@ -93,9 +90,6 @@ def extract_test_feature(image):
         features = []
         roi = (img_seg == label)
         roi_idx = np.nonzero(roi)
-
-        seg_center_x = np.mean(roi_idx[0])
-        seg_center_y = np.mean(roi_idx[1])
 
         seg_size = np.shape(roi_idx)[1]
 
@@ -162,104 +156,32 @@ def extract_trainings():
     print('Data Successfully Saved!')
     return data_features, data_labels
 
-# def training(data, labels):
-
-#     # fix random seed for reproducibility
-#     seed = 7
-#     np.random.seed(seed)
-
-#     # split into test set and training set
-#     X_train, y_train = data,labels
-
-#     # Reshaping the array to 4-dims so that it can work with the Keras API
-#     X_train = X_train.reshape(len(X_train), 28, 28, 1)
-
-#     # Making sure that the values are float so that we can get decimal points after division
-#     X_train = X_train.astype('float32')
-
-#     input_shape = (28, 28, 1)
-#     # create model
-#     model = Sequential()
-#     #add model layers
-
-#     model.add(Conv2D(32, kernel_size=5, activation='relu', input_shape=input_shape))
-#     model.add(MaxPooling2D(pool_size=(2, 2)))
-#     model.add(Conv2D(64, kernel_size=5, activation='relu', input_shape=input_shape))
-#     model.add(MaxPooling2D(pool_size=(2, 2)))
-#     model.add(Flatten()) # Flattening the 2D arrays for fully connected layers
-#     model.add(Dense(1024, activation='relu'))
-#     model.add(Dropout(0.2))
-#     model.add(Dense(10, activation='softmax'))
-
-#     # Compile model
-#     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-#     # Fit the model
-#     history = model.fit(X_train, y_train, validation_split=0.33, epochs=40, batch_size=100,  verbose=2)
-
-#     # save the model for future usage
-#     model_json = model.to_json()
-#     with open("road_detection_cnn_model.json", "w") as json_file:
-#         json_file.write(model_json)
-#     # serialize weights to HDF5
-#     model.save_weights('road_detection_cnn_model.h5')
-#     print("Saved model to disk")
-
-#     # summarize history for accuracy
-#     plt.plot(history.history['accuracy'])
-#     plt.plot(history.history['val_accuracy'])
-#     plt.title('model accuracy')
-#     plt.ylabel('accuracy')
-#     plt.xlabel('epoch')
-#     plt.legend(['train', 'validation'], loc='upper left')
-#     plt.show()
-#     # summarize history for loss
-#     plt.plot(history.history['loss'])
-#     plt.plot(history.history['val_loss'])
-#     plt.title('model loss')
-#     plt.ylabel('loss')
-#     plt.xlabel('epoch')
-#     plt.legend(['train', 'validation'], loc='upper left')
-#     plt.show()
-
 def train(data, labels, svm_path):
-    X_train, X_test, y_train, y_test = train_test_split(data,labels,test_size=0.3)
+    X_train, y_train = data,labels
     print('Start Training process...')
-    svm = LinearSVC(random_state=0, dual= False, multi_class='ovr', class_weight = 'balanced', max_iter= 10000, C= 100.0)
+    svm = LinearSVC(random_state=0, dual= False, multi_class='ovr', class_weight = 'balanced', max_iter= 10000, C= 100.0, tol=1e-5)
     svm.fit(X_train, y_train)
+    print('The accuracy is: ' + str(svm.score(X_train, y_train)))
     joblib.dump(svm, svm_path)
     print('Done training!')
 
 def test_single_data(image):
     data, labels = [], []
+
     if os.path.isfile('training_data_features.npy') and os.path.isfile('training_data_labels.npy'):
         data, labels = load_training_data()
     else: 
         data, labels = extract_trainings()
 
-
-    if not os.path.isfile('road_detection_svm.pkl'):
-        train(data, labels, 'road_detection_svm.pkl')
-
     test_data, img_seg = extract_test_feature(image)
-    svm = joblib.load('road_detection_svm.pkl')
+    train(data, labels, 'road_detection_svm_lsvc.pkl')
+
+    svm = joblib.load('road_detection_svm_lsvc.pkl')
  
     # evaluate loaded model on test data
     predictions = svm.predict(test_data)
 
-    print(predictions)
     return predictions, img_seg
-
-def load_training_model(name):
-    # load json and create model
-    json_file = open(name +'.json', 'r')
-    loaded_model_json = json_file.read()
-    json_file.close()
-    loaded_model = model_from_json(loaded_model_json)
-    # load weights into new model
-    loaded_model.load_weights(name + '.h5')
-    print("Loaded model from disk")
-    return loaded_model
 
 def load_training_data():
     # Load Data
@@ -270,14 +192,58 @@ def load_training_data():
     print('Training Data Successfully Loaded!')
     return data, labels
 
-def load_testing_data():
-    # Load Data
-    data = np.load('testing_data_features.npy', allow_pickle=True)
+def get_segmentation(predictions, img, img_seg):
+    num_labels = np.amax(img_seg)+1
+    blank = np.zeros((img.shape[0],img.shape[1]))
+    for label in range(num_labels):
+        pre = predictions[label]
+        if pre == 1:
+            roi = (img_seg == label)
+            roi_idx = np.nonzero(roi)
+            for idx in range(len(roi_idx[0])):
+                i = roi_idx[0][idx]
+                j = roi_idx[1][idx]
+                blank[i,j] = 10
+    return blank
+
+# see tut7 for reference
+def visualize_segementation(imgLeft, gt_mask):
+    unique, counts = np.unique(gt_mask, return_counts=True)
+    obj_ids = np.unique(gt_mask)
     
-    # Display Message
-    print('Testing Data Successfully Loaded!')
-    return data
+    number_object = obj_ids.shape[0]
+
+    # norm ids
+    count = 0
+    for o_id in obj_ids:
+        gt_mask[gt_mask == o_id] = count
+        count += 1
+
+    base_COLORS = []
+
+    for key, value in mcolors.CSS4_COLORS.items():
+        rgb = webcolors.hex_to_rgb(value)
+        base_COLORS.append([rgb.blue, rgb.green, rgb.red])
+    base_COLORS = np.array(base_COLORS)
+
+    np.random.seed(99)
+    base_COLORS = np.random.permutation(base_COLORS)
+
+    colour_id = np.array([(id) % len(base_COLORS) for id in range(number_object)])
+
+    COLORS = base_COLORS[colour_id]
+    COLORS = np.vstack([[0, 0, 0], COLORS]).astype("uint8")
+    gt_mask = np.int_(gt_mask)
+    mask = COLORS[gt_mask]
+
+    output = ((0.4 * imgLeft) + (0.6 * mask)).astype("uint8")
+    
+    fig=plt.figure(figsize=(10, 10))
+    plt.imshow(output[:,:,::-1])
+    plt.show()
 
 if __name__ == '__main__':
     test = cv2.imread('test/image_left/umm_000061.jpg')
-    test_single_data(test)
+    predictions, img_seg = test_single_data(test)
+    gt_mask_1 = get_segmentation(predictions, test, img_seg)
+    visualize_segementation(test, gt_mask_1)
