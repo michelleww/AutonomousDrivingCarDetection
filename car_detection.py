33a753955b61fb76a16b614b23c02bf98c373206
Arthur_H <arthurh@github.com>
2019-12-02 21:41:44 -0500

    Create car_detection.py

---
+++ car_detection.py
@@ -0,0 +1,88 @@
+import torchvision
+from PIL import *
+import torchvision.transforms as T
+import cv2
+import matplotlib.pyplot as plt
+import os
+
+model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+model.eval()
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+
+
+def get_prediction(img_path, threshold):
+    img = Image.open(img_path) # Load the image
+    transform = T.Compose([T.ToTensor()]) # Defing PyTorch Transform
+    img = transform(img) # Apply the transform to the image
+    pred = model([img]) # Pass the image to the model
+    pred_class = [COCO_INSTANCE_CATEGORY_NAMES[i] for i in list(pred[0]['labels'].numpy())] # Get the Prediction Score
+    pred_boxes = [[(i[0], i[1]), (i[2], i[3])] for i in list(pred[0]['boxes'].detach().numpy())] # Bounding boxes
+    pred_score = list(pred[0]['scores'].detach().numpy())
+    pred_t = [pred_score.index(x) for x in pred_score if x > threshold][-1] # Get list of index with score greater than threshold.
+    pred_boxes = pred_boxes[:pred_t+1]
+    pred_class = pred_class[:pred_t+1]
+    return pred_boxes, pred_class
+
+
+def object_detection_api(img_path, threshold=0.6, rect_th=1, text_size=0.7, text_th=1):
+    try:
+        want_detect = ['car', 'bus', 'truck']
+        boxes, pred_cls = get_prediction(img_path, threshold) # Get predictions
+        img = cv2.imread(img_path) # Read image with cv2
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB) # Convert to RGB
+        for i in range(len(boxes)):
+            if pred_cls[i] in want_detect:
+                cv2.rectangle(img, boxes[i][0], boxes[i][1],color=(0, 255, 0), thickness=rect_th) # Draw Rectangle with the coordinates
+                cv2.putText(img,pred_cls[i], boxes[i][0],  cv2.FONT_HERSHEY_SIMPLEX, text_size, (0,255,0),thickness=text_th) # Write the prediction class
+        plt.figure(figsize=(20,30)) # display the output image
+        plt.imshow(img)
+        plt.xticks([])
+        plt.yticks([])
+        #plt.show()
+        plt.savefig(".\\train\\results-dets\\"+img_path[-14:])
+        return boxes[i][0]
+    except:
+        print("No vehicle detected in: " + str(img_path))
+        img = cv2.imread(img_path)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        plt.figure(figsize=(20,30))
+        plt.imshow(img)
+        plt.savefig(".\\train\\results-dets\\"+img_path[-14:])
+        return None
+
+
+
+# # Detect test folder
+# train_dir
+# = ".\\test\\image_left\\"
+# for each_image in os.listdir(train_dir
+#):
+#     if each_image.endswith(".jpg"):
+#         # save all images
+#         print("Detecting image: " + str(each_image))
+#         object_detection_api(train_dir
+#+str(each_image))
+
+
+# Detect train\image_left folder
+train_dir = ".\\train\\image_left\\"
+for each_image in os.listdir(train_dir):
+    if each_image.endswith(".jpg"):
+        # save all images
+        print("Detecting image: " + str(each_image))
+        object_detection_api(train_dir
+    +str(each_image))
