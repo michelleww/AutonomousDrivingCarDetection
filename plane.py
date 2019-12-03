f618ab8426a855855602f53251081275d1781f01
Arthur_H <arthurh@github.com>
2019-12-03 00:26:05 -0500

    Update plane.py

---
+++ plane.py
@@ -0,0 +1,20 @@
+import numpy as np
+import scipy as sp
+import cv2
+from matplotlib import pyplot as plt
+from disparity import *
+from skimage import io
+from disparity import *
+from depth import *
+from compute3d import *
+
+def fit_plane():
+    pass
+
+
+def plot_3d_cloud_pt():
+    pass
+
+
+if __name__ == "__main__":
+    pass
