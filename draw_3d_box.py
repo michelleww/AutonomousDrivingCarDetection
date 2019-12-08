88e4c8554549be1a8e8a95cfe4671756fbd71bbc
Luya Wang <luya.wang@mail.utoronto.ca>
2019-12-08 15:48:37 -0500

    update

--- draw_3d_box.py
+++ draw_3d_box.py
@@ -1,37 +1,50 @@
-from sklearn import linear_model, datasets
-from skimage.measure import LineModelND, ransac
-import scipy.linalg
+
 import open3d
-import random
-from numpy.linalg import svd
 import math
 from car_detection import object_detection_api
 from draw_car_viewPoint import create_output_directory
 from plane2 import main
 import cv2
 
-def draw_3d_boxes(boxes, recursive_glob_path, img_path, svm_path):
-    resize_info = get_resize_info(recursive_glob_path)
+def draw_3d_boxes(boxes, plane_model, data, img_path):
+    lines = [[0, 1], [0, 2], [1, 3], [2, 3], [4, 5], [4, 6], [5, 7], [6, 7], [0, 4], [1, 5], [2, 6], [3, 7]]
+    colors = [[1, 0, 0] for i in range(len(lines))]
+    boxes_3d = []
+
     img_gray = cv2.imread(img_path, 0)
-    img = cv2.imread(img_path)
+    height, widht = img_gray.shape
+
     for box in boxes:
+        points = []
         (left, top), (right, bottom) = box[0], box[1]
-        img_seg = img_gray[int(round(top)):int(round(bottom))+1, int(round(left)):int(round(right))+1]
-        angle = int(test_single_image(img_seg, resize_info, svm_path))
 
-        center_point_x = (right + left)/2
-        center_point_y = (bottom + top)/2
-        length  = (bottom - top)/2 + 15
-        p2_x =  int(round(center_point_x + length * math.cos(angle * np.pi / 180.0)))
-        p2_y =  int(round(center_point_y + length * math.sin(angle * np.pi / 180.0)))
+        center_x = int(round((right + left)/2))
+        center_y = int(round((bottom + top)/2))
 
-        # Draw Rectangle with the coordinates and put detection class on the left top cornor
-        cv2.rectangle(img, box[0], box[1],color=(0, 255, 0), thickness=1)
-        cv2.putText(img,'Car', box[0],  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,255,0),thickness=2)
-        cv2.arrowedLine(img, (int(center_point_x), int(center_point_y)), (p2_x, p2_y), (210, 142, 40), thickness=3)
-        cv2.putText(img,str(angle), (int(center_point_x), int(center_point_y)),  cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255),thickness=2)
-    cv2.imwrite('./detected_car/results-dets/' + test_img[(test_img.index('/u')+1):], img)
-    return img
+        center_3d = data[center_y*widht+center_x]
+        
+        x= center_3d[0]
+        y= center_3d[1]
+        z = center_3d[2]
+        z_prime = (-plane_model[0]*x - plane_model[1]*y - plane_model[3])/plane_model[2]
+
+        z_dis = abs(z - z_prime)
+        x_dis = abs(center_x - left)
+        y_dis = abs(center_y - bottom)
+
+        z_dis = 0.5
+        x_dis = 0.5
+        y_dis = 0.4
+
+        points.extend([[x-x_dis, y-y_dis,z+z_dis], [x+x_dis, y-y_dis,z+z_dis], [x-x_dis, y+y_dis,z+z_dis], [x+x_dis, y+y_dis,z+z_dis]])
+        points.extend([[x-x_dis, y-y_dis,z-z_dis], [x+x_dis, y-y_dis,z-z_dis], [x-x_dis, y+y_dis,z-z_dis], [x+x_dis, y+y_dis,z-z_dis]])
+
+        line_set = open3d.geometry.LineSet()
+        line_set.points = open3d.utility.Vector3dVector(points)
+        line_set.lines = open3d.utility.Vector2iVector(lines)
+        line_set.colors = open3d.utility.Vector3dVector(colors)
+        boxes_3d.append(line_set)
+    return boxes_3d
 
 
 if __name__ == "__main__":
@@ -40,12 +53,13 @@ if __name__ == "__main__":
     calib_dir = "train/calib/um_000088.txt"
     create_output_directory()
     cars = object_detection_api(left_im_dir)
-        # left image
+    # left image
     test_left = cv2.imread(left_im_dir)
 
     # right image
     test_right = cv2.imread(right_im_dir)
-    pcd, data, plane_model = main(test_left, test_right, calib_dir)
-    
+    pcd, data, plane_model = main(left_im_dir, test_left, test_right, calib_dir)
+    line_sets = draw_3d_boxes(cars, plane_model, data, left_im_dir)
+    pcd.extend(line_sets)
     open3d.visualization.draw_geometries(pcd)
 
