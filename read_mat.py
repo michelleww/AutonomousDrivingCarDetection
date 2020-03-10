c4505e689648029587d4ecaf6654994677040f4f
Luya Wang <luya.wang@mail.utoronto.ca>
2020-03-10 04:56:37 -0400

    delete unnecessary files

--- read_mat.py
+++
@@ -1,9 +0,0 @@
-from scipy.io import loadmat
-
-if __name__ == "__main__":
-    dir = "models_lsvm\car_2_2012_10_04-20_13_57_model.mat"
-    mat = loadmat(dir)
-    # lon = mat['lon']
-    # lat = mat['lat']
-    # print(loadmat(dir)['lon'])
-    print(mat)
